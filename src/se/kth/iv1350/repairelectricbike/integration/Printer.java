377f5270d2745b9497195c0e4f2c0667e4fff055
ckaur <ckaur@kth.se>
2026-04-27 11:57:52 +0200

    Printer implemented

--- src/se/kth/iv1350/repairelectricbike/integration/Printer.java
+++ src/se/kth/iv1350/repairelectricbike/integration/Printer.java
@@ -1,5 +1,18 @@
 package se.kth.iv1350.repairelectricbike.integration;
 
-public class Printer {
+import se.kth.iv1350.repairelectricbike.model.RepairOrder;
 
+/**
+ * The interface to the printer, used for all printouts initiated by this program.
+ */
+
+public class Printer {
+    /**
+     * Prints the specified repairOrder. This dummy implementation prints to <code>System.out</code> instead of a printer.
+     * 
+     * @param repairOrder
+     */
+    public void printRepairOrder(RepairOrderDTO repairOrder) {
+        System.out.println(repairOrder.createRepairOrderString());
+    }
 }
