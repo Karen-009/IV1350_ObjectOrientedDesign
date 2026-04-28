6c47568fe820cdba2230cf0c676a355631404587
ckaur <ckaur@kth.se>
2026-04-28 15:11:48 +0200

    lagt till systemoperationer

--- src/se/kth/iv1350/repairelectricbike/controller/Controller.java
+++ src/se/kth/iv1350/repairelectricbike/controller/Controller.java
@@ -1,11 +1,15 @@
 package se.kth.iv1350.repairelectricbike.controller;
 
 import se.kth.iv1350.repairelectricbike.model.RepairOrder;
+
 import se.kth.iv1350.repairelectricbike.integration.RepairOrderDTO;
+import se.kth.iv1350.repairelectricbike.model.DiagnosticTaskDTO;
+import se.kth.iv1350.repairelectricbike.model.Bike;
 import se.kth.iv1350.repairelectricbike.integration.CustomerRegistry;
 import se.kth.iv1350.repairelectricbike.integration.RepairOrderRegistry;
 import se.kth.iv1350.repairelectricbike.integration.Printer;
 import se.kth.iv1350.repairelectricbike.integration.RegistryCreator;
+import se.kth.iv1350.repairelectricbike.integration.CustomerDTO;
 /**
  * This is the application's only controller class. All calls to the model pass through here.
  */
@@ -28,13 +32,26 @@ public class Controller {
         this.printer = printer;
     }
 
+    public RepairOrderDTO createRepairOrder(String problemDesc, String customerPhone, Bike bikeSerialNo) {
+        repairOrder = new RepairOrder(problemDesc, customerPhone, bikeSerialNo);
+        RepairOrderDTO dto = repairOrder.getRepairOrderDTO();
+        repairOrderRegistry.saveRepairOrder(dto);
+        return dto;
+    }
+
+    public CustomerDTO findCustomer(String phoneNumber) {
+        return CustomerRegistry.findCustomer(phoneNumber);
+    }
+
+    
+
     /**
      * Accepts a specific repair order.
      * 
      * @param repairOrderId The ID of the repair order to accept.
      */
 
-    public static void acceptRepairOrder(String repairOrderID) {
+    public void acceptRepairOrder(String repairOrderID) {
         RepairOrder repairOrder = repairOrderIDRegistry.getRepairOrder(repairOrderID);
         repairOrder.acceptRepairOrder();
 
