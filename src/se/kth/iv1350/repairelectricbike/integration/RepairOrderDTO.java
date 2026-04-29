893ec0b55b98d1fe002ec81ce92de3c6a83b14cd
Elias Malekdanesh <eliasmal@kth.se>
2026-04-29 23:00:31 +0200

    Update RepairOrderDTO
    Updated RepairOrderDTO with order info

--- src/se/kth/iv1350/repairelectricbike/integration/RepairOrderDTO.java
+++ src/se/kth/iv1350/repairelectricbike/integration/RepairOrderDTO.java
@@ -9,6 +9,7 @@ package se.kth.iv1350.repairelectricbike.integration;
  */
 public final class RepairOrderDTO {
     private final String id;
+    private final String phoneNumber;
     private final String date;
     private final String customersProblemDescription;
     private final String estimatedCompletionDate;
@@ -18,31 +19,66 @@ public final class RepairOrderDTO {
      * Creates a new instance.
      * 
      * @param id                          The repair order ID.
+     * @param phoneNumber                 The customers phone number.
      * @param date                        The date the order was created.
      * @param customersProblemDescription Description provided by customer.
      * @param estimatedCompletionDate     Expected finish date.
      * @param state                       The current status of the repair.
      */
 
-    public RepairOrderDTO(String id, String date, String customersProblemDescription,
+    public RepairOrderDTO(String id, String phoneNumber, String date, String customersProblemDescription,
             String estimatedCompletionDate, RepairOrderState state) {
         this.id = id;
+        this.phoneNumber = phoneNumber;
         this.date = date;
         this.customersProblemDescription = customersProblemDescription;
         this.estimatedCompletionDate = estimatedCompletionDate;
         this.state = state;
     }
 
+    /**
+     * @return The repair order ID.
+     */
+
     public String getId() {
         return id;
     }
+
+    /**
+     * @return The customer's phone number.
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @return The date the order was created.
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @return The description of the customers problem.
+     */
+    public String getCustomersProblemDescription() {
+        return customersProblemDescription;
+    }
+
+    /**
+     * @return The estimated completion date.
+     */
+    public String getEstimatedCompletionDate() {
+        return estimatedCompletionDate;
+    }
+
+    /**
+     * @return The current status of the repair.
+     */
 
     public RepairOrderState getState() {
         return state;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
+    
 }
