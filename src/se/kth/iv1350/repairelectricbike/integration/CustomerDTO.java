53bdb5430ee24ac266c79252cc2d62b14d719a68
ckaur <ckaur@kth.se>
2026-04-29 22:45:14 +0200

    Implemented CustomerRegistry

--- src/se/kth/iv1350/repairelectricbike/integration/CustomerDTO.java
+++ src/se/kth/iv1350/repairelectricbike/integration/CustomerDTO.java
@@ -10,22 +10,51 @@ public final class CustomerDTO {
     private final String bikeBrand;
     private final String bikeModel;
     private final String bikeSerialNo;
+    private final String phoneNumber;
 
     /**
      * Creates a new instance.
      * 
      * @param name          The customer name.
-     * @param email         The date the order was created.
+     * @param email         The customer email address.
      * @param bikeBrand     Description of the customers bikebrand.
      * @param bikeModel     Description of the customers bike model.
      * @param bikeSeriallNo The customer bikes serial number.
      */
 
-    public CustomerDTO(String name, String email, String bikeBrand, String bikeModel, String bikeSeriallNo) {
+    public CustomerDTO(String name, String email, String bikeBrand, String bikeModel, String bikeSeriallNo, String phoneNumber) {
         this.name = name;
         this.email = email;
         this.bikeBrand = bikeBrand;
         this.bikeModel = bikeModel;
         this.bikeSerialNo = bikeSeriallNo;
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return The customer's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return The customer's phone number.
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @return The customer's email address.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer: " + name + ", Phone: " + phoneNumber
+               + ", Bike: " + bikeBrand + " " + bikeModel;
     }
 }
