a08997ddd3cd6005e19b990f340d904862c46e0a
Karen <141221757+Karen-009@users.noreply.github.com>
2026-05-01 16:56:28 +0200

    Added the PrinterTest

--- tests/se/kth/iv1350/repairelectricbiketest/integration/PrinterTest.java
+++ tests/se/kth/iv1350/repairelectricbiketest/integration/PrinterTest.java
@@ -1,5 +1,34 @@
 package IV1350_ObjectOrientedDesign.tests.se.kth.iv1350.repairelectricbiketest.integration;
 
-public class PrinterTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.kth.iv1350.repairelectricbike.integration.Printer;
+import se.kth.iv1350.repairelectricbike.integration.RepairOrderDTO;
+import se.kth.iv1350.repairelectricbike.integration.RepairOrderState;
 
+public class PrinterTest {
+    private Printer printer;
+
+    @BeforeEach
+    public void setUp() {
+        printer = new Printer();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        printer = null;
+    }
+
+    @Test
+    public void testPrintRepairOrder() {
+        RepairOrderDTO dto = new RepairOrderDTO(
+                "REQ-123",
+                "2024-05-20",
+                "Broken chain",
+                "2024-05-22",
+                RepairOrderState.ACCEPTED);
+
+        printer.printRepairOrder(dto);
+    }
 }
